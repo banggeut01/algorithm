423bfe4e886f48f2368a7da32f6d88dd2961af0e
banggeut01 <genie121110@gmail.com>
2019-07-31 16:06:35 +0900

    190731 | 리드미, D1 코드

---
+++ code/1206.py
@@ -0,0 +1,15 @@
+# View
+# import sys
+# sys.stdin = open('1206input.txt', 'r')
+
+for tc in range(10):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    cnt = 0
+    for i in range(2, N-2): # N-4번
+        if arr[i] > arr[i-1] and arr[i] > arr[i-2]: # 왼쪽 검사
+            tmp1 = min(arr[i] - arr[i-1], arr[i] - arr[i-2])
+            if arr[i] > arr[i+1] and arr[i] > arr[i+2]: # 오른쪽 검사
+                tmp2 = min(arr[i] - arr[i+1], arr[i] - arr[i+2])
+                cnt += min(tmp1, tmp2)
+    print('#{} {}'.format(tc + 1, cnt))

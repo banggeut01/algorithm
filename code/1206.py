4e3c1aad7671d568f1728bd01aba86f70af9cad6
banggeut01 <genie121110@gmail.com>
2019-07-31 16:23:43 +0900

    1206.py 수정

--- code/1206.py
+++ code/1206.py
@@ -8,8 +8,14 @@ for tc in range(10):
     cnt = 0
     for i in range(2, N-2): # N-4번
         if arr[i] > arr[i-1] and arr[i] > arr[i-2]: # 왼쪽 검사
-            tmp1 = min(arr[i] - arr[i-1], arr[i] - arr[i-2])
+            # tmp1 = min(arr[i] - arr[i-1], arr[i] - arr[i-2])
+            tmp1 = arr[i] - arr[i-2] if arr[i]-arr[i-1] > arr[i] - arr[i-2] else arr[i]-arr[i-1]
             if arr[i] > arr[i+1] and arr[i] > arr[i+2]: # 오른쪽 검사
-                tmp2 = min(arr[i] - arr[i+1], arr[i] - arr[i+2])
-                cnt += min(tmp1, tmp2)
+                # tmp2 = min(arr[i] - arr[i+1], arr[i] - arr[i+2])
+                tmp2 = arr[i] - arr[i+2] if arr[i] - arr[i+1] > arr[i] - arr[i+2] else arr[i] - arr[i+1]
+                if tmp1 > tmp2:
+                    cnt += tmp2
+                else:
+                    cnt += tmp1
+                # cnt += min(tmp1, tmp2)
     print('#{} {}'.format(tc + 1, cnt))

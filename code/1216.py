4b51e1259a6b293ca4f477ac697ffa3415a29e06
banggeut01 <genie121110@gmail.com>
2019-08-19 17:55:58 +0900

    1216회문2풀이중

--- code/1216.py
+++ code/1216.py
@@ -2,12 +2,40 @@
 import sys
 sys.stdin = open('1216input.txt', 'r')
 
+def row_pal(i, j, m):
+    p = 0 # p: 회문 idx
+    while p < m // 2:
+        if word[i][j + p] == word[i][j + m - p - 1]:
+            p += 1
+        else:
+            return 0
+    return m
 
+def col_pal(i, j, m):
+    p = 0 # p: 회문 idx
+    while p < m // 2:
+        if word[i + p][j] == word[i + m - p - 1][j]:
+            p += 1
+        else:
+            return 0
+    return m
 
 for t in range(10):
     tc = int(input())
-    grid = [input() for _ in range(100)]
+    word = [input() for _ in range(100)]
 
+    max_len = 0
+    for i in range(100):
+        for j in range(99): # j인덱스 0 ~ 98, 회문길이 100 ~ 2
+            for m in range(100, 99-j, -1): # 회문 길이 100 ~ (99-j), 길이1이면 1출력
+                tmp = row_pal(i, j, m)
+                if tmp > max_len:
+                    max_len = tmp
+                tmp = col_pal(j, i, m)
+                if tmp > max_len:
+                    max_len = tmp
+
+    print('#{} {}'.format(tc, max_len))
 
 
 

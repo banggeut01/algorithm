d2d0b55b3e09352f657293a6929767a5a83ef996
banggeut01 <genie121110@gmail.com>
2019-10-14 17:54:53 +0900

    나무재테크 풀이중

--- code/A/16235.py
+++ code/A/16235.py
@@ -2,63 +2,55 @@
 
 import collections
 N, M, K = map(int, input().split()) # N: 땅크기, M: 나무 개수, K: 연수
-tree = collections.deque()
-# die = collections.deque()
+tree = [[collections.deque() for _ in range(N + 1)] for _ in range(N + 1)] # 나이 오름차순
 board = [[5] * (N + 1) for _ in range(N + 1)]
 A = [[0]] + [[0] + list(map(int, input().split())) for _ in range(N)] # 양분
-move = [[0] * (N + 1) for _ in range(N + 1)]
+move = [[0] * (N + 1) for _ in range(N + 1)] # 죽은 나무 => 양분
+tcnt = 0
 for _ in range(M):
     i, j, a = map(int, input().split()) # 좌표, 나이
-    tree.append((i, j, a))
-tree = collections.deque(sorted(tree)) # 정렬
-tree.append((0, 0, 0))
-cnt = 0
+    tree[i][j].append(a)
+    tcnt += 1
+year = 0
+
 while True:
     # 봄
-    i, j, a = tree.popleft() #### 비었는지 검사
-    while i + j + a != 0:
-        if board[i][j] >= a: # 나이먹기
-            board[i][j] -= a
-            a += 1
-            tree.append((i, j, a))
-        else: # die
-            # die.append((i, j, a // 2))
-            move[i][j] = a // 2
-        i, j, a = tree.popleft()
-    # print('봄')
-    # print(tree)
-    tree.append((0, 0, 0))
-
-    # 여름
-    # while die:
-    #     i, j, a = die.popleft()
-    #     board[i][j] += a
     for i in range(1, N + 1):
         for j in range(1, N + 1):
-            board[i][j] += move[i][j]
-            move[i][j] = 0
+            if not tree[i][j]: continue
+            x = 0
+            while x < len(tree[i][j]) and board[i][j] >= tree[i][j][x]:
+                board[i][j] -= tree[i][j][x]
+                x += 1
+            while x != len(tree[i][j]):
+                move[i][j] += tree[i][j][x] // 2
+                tree[i][j].pop()
+                tcnt -= 1
+    # 여름
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if move[i][j]:
+                board[i][j] += move[i][j]
+                move[i][j] = 0
     # 가을
-    i, j, a = tree.popleft()
-    while i + j + a != 0:
-        if a and not a % 5: # 번식
-            for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (1, -1), (-1, 1), (-1, -1):
-                r, c = i + dx, j + dy
-                if 0 < r < N + 1 and 0 < c < N + 1:
-                    tree.append((r, c, 1))
-        tree.append((i, j, a))
-        i, j, a = tree.popleft()
-    # print('가을')
-    # print(tree)
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if not tree[i][j]: continue
+            # for x in range(len(tree)):
+                for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (1, -1), (-1, 1), (-1, -1):
+                    r, c = i + dx, j + dy
+                    if 0 < r < N + 1 and 0 < c < N + 1:
+                        tree[r][c].appendleft(1)
+                        tcnt += 1
     # 겨울
     for i in range(1, N + 1):
         for j in range(1, N + 1):
             board[i][j] += A[i][j]
-
-    cnt += 1
-    if cnt == K: # K년
-        break
-    if not len(tree): # 산 나무 없으면,
-        break
-    tree = collections.deque(sorted(tree)) # 정렬
-    tree.append((0, 0, 0))
-print(len(tree))
+    break
+print(tree)
+    # year += 1
+    # if year == K: # K년
+    #     break
+    # if not tcnt: break
+print(tcnt)
+print(tree)

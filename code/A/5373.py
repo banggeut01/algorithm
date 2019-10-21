033591bcaf47ad5b7156b6359656ec162f794477
banggeut01 <genie121110@gmail.com>
2019-10-21 16:48:17 +0900

    큐빙 풀이중

--- code/A/5373.py
+++ code/A/5373.py
@@ -1,21 +1,64 @@
 # 큐빙 5373.py
+# U: 윗 면, D: 아랫 면, F: 앞 면, B: 뒷 면, L: 왼쪽 면, R: 오른쪽 면
+# '-': 반시계 방향, '+': 시계 방향
+def rotationLeft(x, y):
+    tmp = [[0] * 9 for _ in range(12)]
+    # 반시계방향
+    for i in range(x, x + 3):
+        for j in range(y, y + 3):
+            tmp[x][y] = cube[y][3 - 1 - x]
+    for i in range(x, x + 3):
+        for j in range(y, y + 3):
+            cube[x][y] = tmp[x][y]
 
-cube = [[['w'] * 3 for _ in range(3)], # 0: 윗면
-        [['y'] * 3 for _ in range(3)], # 1: 아랫면
-        [['r'] * 3 for _ in range(3)], # 2: 앞면
-        [['o'] * 3 for _ in range(3)], # 3: 뒷면
-        [['g'] * 3 for _ in range(3)], # 4: 왼쪽면
-        [['b'] * 3 for _ in range(3)],] # 5: 오른쪽면\
+def rotationRight(x, y):
+    tmp = [[0] * 9 for _ in range(12)]
+    # 반시계방향
+    for i in range(x, x + 3):
+        for j in range(y, y + 3):
+            tmp[x][y] = cube[3 - 1 - y][x]
+    for i in range(x, x + 3):
+        for j in range(y, y + 3):
+            cube[x][y] = tmp[x][y]
+
+def rotationCube(plane, dir): # 돌린 면, 회전 방향
+    if plane == 'L':
+        if dir == '-': # [3]
+            tmp = cube[0][:3]
+            for x in range(9):
+                cube[x][3] = cube[x + 1][3]
+            for x in range(3)
+            cube[11][3] = tmp
+            rotationLeft(3, 3)
+        else:
+            tmp = cube[11][3]
+            for x in range(11):
+                cube[x + 1][3] = cube[x][3]
+            cube[0][3] = tmp
+            rotationRight(3, 3)
+
+cube = [[0, 0, 0, 'o', 'o', 'o', 0, 0, 0],              # [0][0~9]
+        [0, 0, 0, 'o', 'o', 'o', 0, 0, 0],
+        [0, 0, 0, 'o', 'o', 'o', 0, 0, 0],
+        ['g', 'g', 'g', 'w', 'w', 'w', 'b', 'b', 'b'],  # [3]
+        ['g', 'g', 'g', 'w', 'w', 'w', 'b', 'b', 'b'],
+        ['g', 'g', 'g', 'w', 'w', 'w', 'b', 'b', 'b'],
+        [0, 0, 0, 'r', 'r', 'r', 0, 0, 0],              # [6]
+        [0, 0, 0, 'r', 'r', 'r', 0, 0, 0],
+        [0, 0, 0, 'r', 'r', 'r', 0, 0, 0],
+        [0, 0, 0, 'y', 'y', 'y', 0, 0, 0],              # [9]
+        [0, 0, 0, 'y', 'y', 'y', 0, 0, 0],
+        [0, 0, 0, 'y', 'y', 'y', 0, 0, 0]]
+
 
 t = int(input())
 for _ in range(t):
     tc = int(input())
     act = list(input().split())
-    a, b = 'L', '-'
-    if a == 'L':
-        if b == '-':
-            
-            cube[0][]
-print(''.join(cube[0][0]))
-print(''.join(cube[0][1]))
-print(''.join(cube[0][2]))
+    print(act)
+    for idx in range(len(act)):
+        rotationCube(act[idx][0], act[idx][1])
+    for i in range(3, 6):
+        print(''.join(map(str, cube[i][3:6])))
+    import pprint
+    pprint.pprint(cube)
